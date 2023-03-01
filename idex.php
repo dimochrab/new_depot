2fd88e8e57bb4ec0e6a53fec23fe74f9410440d6
dimochrab <ademkhdher@gmail.com>
2023-03-01 16:37:53 +0100

    ajout de paragraphe

--- idex.php
+++ idex.php
@@ -1,2 +1,3 @@
 <h1>ello
 </h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, soluta libero asperiores suscipit temporibus at iusto molestias omnis vitae maiores amet blanditiis consequuntur quia deserunt ab. Hic soluta est similique!</p>
